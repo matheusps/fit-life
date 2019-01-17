b786ad5fe00b6700a0a8960aeab5922763bf9f3a
matheusps <matheus.ps@icloud.com>
2019-01-16 22:37:10 -0300

    Boilerplate

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import app from './style.module.css';
+
+const App = () => (
+  <div className={app.container}>
+    <div className={app.content}></div>
+  </div>
+)
+
+export default App;
